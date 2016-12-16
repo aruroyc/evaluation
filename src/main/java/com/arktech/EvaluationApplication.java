b36f73719ad1b5cde248e022b623e528b278998d
Koustav Ray <koustavtub@gmail.com>
2016-12-17 01:44:08 +0530

    Initial Push Of Project
    Spring Modules Added:
    --Web
    --JPA
    --Mongo
    --Security

---
+++ src/main/java/com/arktech/EvaluationApplication.java
@@ -0,0 +1,12 @@
+package com.arktech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluationApplication.class, args);
+	}
+}
