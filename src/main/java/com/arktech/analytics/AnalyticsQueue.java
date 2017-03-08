558e7d180bb1f73089466a8b0e2728e45e12a3c1
aruroyc <arunava.roychoudhury@here.com>
2017-03-08 14:19:01 +0530

    Analytics asynchronous queue processing.

---
+++ src/main/java/com/arktech/analytics/AnalyticsQueue.java
@@ -0,0 +1,78 @@
+package com.arktech.analytics;
+
+import com.arktech.analytics.api.DataProcessor;
+import com.arktech.analytics.persistence.NoSQLDataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.Serializable;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by roychoud on 07/03/17.
+ */
+@Service
+public class AnalyticsQueue {
+
+    @Value("${analytics.consumers}")
+    private int maxAnalyticsConsumers;
+
+    @Autowired
+    private NoSQLDataRepository repository;
+    @Autowired
+    private static List<DataProcessor> dataProcessors;
+
+    private Map<String,DataProcessor> dataProcessorMap = new LinkedHashMap<>();
+    private ExecutorService executorService;
+
+    @PostConstruct
+    private void init() {
+        if (dataProcessorMap.isEmpty())
+            dataProcessors.forEach(dataProcessor -> {
+                dataProcessorMap.put(dataProcessor.getClass().getAnnotation(Qualifier.class).value(), dataProcessor);
+            });
+        executorService= Executors.newFixedThreadPool(maxAnalyticsConsumers);
+    }
+
+    public void pushData(Object object,String analyticsFacet,boolean doProcess)
+    {
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    repository.save((Serializable)(doProcess?dataProcessorMap.get
+                            (analyticsFacet)
+                            .doProcess(object):object));
+                }
+                catch (Exception e)
+                {
+                    //TODO Logging
+                }
+            }
+        });
+
+    }
+    public void pushData(Collection<?> object,String analyticsFacet,boolean doProcess)
+    {
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    repository.save((Collection<?>)(doProcess?dataProcessorMap.get
+                            (analyticsFacet)
+                            .parallelProcess(object):object));
+                }
+                catch (Exception e)
+                {
+                    //TODO Logging
+                }
+            }
+        });
+    }
+
+}
